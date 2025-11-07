5a1029cf2734180f92041d11636e1fd233c51528
e22500013 <anis.lafrad@etudiant.univ-brest.fr>
2025-11-07 15:00:55 +0100

    Modification list.h

--- list.c
+++ list.c
@@ -1,9 +1,12 @@
 #include "list.h"
 
-void list_create (void){
+node_t* list_create (void){
     node_t n;
+    return &n;
 }
+void list_get_data (const node_t* node){
 
+}
 void list_set_data (node_t* node, void* data){
     node->data = data;
     node->next = node;
@@ -24,6 +27,11 @@ node_t list_remove (node_t* head, void* data){
 
 
 }
+node_t* list_headRemove (node_t* head){
+
+
+}
+
 void list_destroy (node_t* head){
 
 }
--- list.h
+++ list.h
@@ -7,12 +7,14 @@ typedef struct node {
 }node_t;
 
 
-void list_create (void);
+node_t* list_create (void);
+void* list_get_data (const node_t* node);
 void list_set_data (node_t* node, void* data); 
-node_t list_next (node_t* node);
-node_t list_insert (node_t* head, void* data);
-node_t list_append (node_t*head, void* data);
-node_t list_remove (node_t* head, void* data); 
+node_t* list_next (node_t* node);
+node_t* list_insert (node_t* head, void* data);
+node_t* list_append (node_t* head, void* data);
+node_t* list_remove (node_t* head, void* data); 
+node_t* list_headRemove (node_t* head); 
 void list_destroy (node_t* head);
 
 #endif
