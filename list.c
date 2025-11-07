17b77d8edaab72f8bda1da7f40cfe0924082025f
GROSGASNIER <33xflbhj@gmail.com>
2025-11-07 15:06:23 +0100

    mise en page + list_next

--- list.c
+++ list.c
@@ -4,29 +4,35 @@ node_t* list_create (void){
     node_t n;
     return &n;
 }
-void list_get_data (const node_t* node){
+
+void* list_get_data (const node_t* node){
 
 }
+
 void list_set_data (node_t* node, void* data){
     node->data = data;
     node->next = node;
 }
-node_t list_next (node_t* node){
+
+node_t* list_next (node_t* node){
+    return node->next;
+}
+
+node_t* list_insert (node_t* head, void* data){
 
 
 }
-node_t list_insert (node_t* head, void* data){
 
-
-}
-node_t list_append (node_t*head, void* data){
+node_t* list_append (node_t*head, void* data){
 
     
 }
-node_t list_remove (node_t* head, void* data){
+
+node_t* list_remove (node_t* head, void* data){
 
 
 }
+
 node_t* list_headRemove (node_t* head){
 
 
