980042589ecfe04492eb56bb9be761d593cd5006
GROSGASNIER <33xflbhj@gmail.com>
2025-11-07 15:46:13 +0100

    le node est bien initialisé

--- list.c
+++ list.c
@@ -1,44 +1,48 @@
 #include "list.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-node_t* list_create (void){
-    node_t* n;
+node_t* list_create (void) {
+    node_t* n = malloc(sizeof(node_t));
+
     n->data = NULL;
+    n->next = NULL;
+
     return n;
 }
 
-void* list_get_data (const node_t* node){
+void* list_get_data (const node_t* node) {
     return node->data;
 }
 
-void list_set_data (node_t* node, void* data){
+void list_set_data (node_t* node, void* data) {
     node->data = data;
 }
 
-node_t* list_next (node_t* node){
+node_t* list_next (node_t* node) {
     return node->next;
 }
 
-node_t* list_insert (node_t* head, void* data){
+node_t* list_insert (node_t* head, void* data) {
 
 
 }
 
-node_t* list_append (node_t*head, void* data){
+node_t* list_append (node_t*head, void* data) {
 
     
 }
 
-node_t* list_remove (node_t* head, void* data){
+node_t* list_remove (node_t* head, void* data) {
 
 
 }
 
-node_t* list_headRemove (node_t* head){
+node_t* list_headRemove (node_t* head) {
 
 
 }
 
-void list_destroy (node_t* head){
+void list_destroy (node_t* head) {
 
 }
--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "list.h"
+#include <stdlib.h>
 
 int main()
 {
     node_t* list = list_create();
     int a = 25;
     int *p = &a;
-    list_set_data (list, p);
-    //printf("%p",list->data);
-    printf("%p",list);
+    list_set_data(list, p);
+    printf("Adresse du noeud : %p\n", (void*)list);
+    printf("Adresse stockée dans data : %p\n", list->data);
+    printf("Valeur pointée : %d\n", *(int*)list->data);
+    free(list);
     return 0;
 }
