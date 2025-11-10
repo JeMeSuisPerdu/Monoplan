2b705db586e37e1c3982aec5951644306dc8ff9f
GROSGASNIER <33xflbhj@gmail.com>
2025-11-10 12:17:01 +0100

    écriture des structures cell et token dans cell.h

---
+++ mod_cell_form/cell.h
@@ -0,0 +1,48 @@
+/** 
+ * @headerfile list.h
+ * @brief 
+ */
+
+#ifndef CELL
+#define CELL
+
+#include "../stack/stack.h"
+#include <stdlib.h>
+
+/** 
+ * @struct token
+ * @brief Représente un jeton d'une cellule
+ * 
+ *
+ *
+ */
+typedef struct token {
+    enum {VALUE, REF, OPERATOR} type ;
+    union {
+        double cst;
+        s_cell* ref;
+        void (*operator) (my_stack_t* eval); 
+    } value ;
+} s_token ;
+
+/** 
+ * @struct cell
+ * @brief Représente une cellule du tableur
+ * 
+ * 
+ * @code 
+    Création d’un tableau de 3 tokens
+    cell->token_count = 3;
+    cell->tokens = malloc(cell->token_count * sizeof(s_token));
+  @endcode
+ */
+typedef struct cell {
+    char* content; /**  */
+    double value; /**  */
+    s_token* tokens; /** Tableau alloué dynamiquement ; Jetons créés à partir de l'analyse du contenu */
+    size_t token_count; /** Nombre de jetons dans le tableau */
+    struct cell** dependant_cells; /** Tableau de pointeurs des cellules qui dépendent de cette cellule */
+    size_t dependant_cells_count; /** Nombre de cellules dépendantes */
+} s_cell ;
+
+#endif
